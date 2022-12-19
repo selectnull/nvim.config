89bf81a82fa174d788510613eaa22cd0e2565f51
Sasha Matijasic <sasha@selectnull.com>
2022-12-19 19:54:36 +0100

    LSP typescript

--- config/nvim/lua/selectnull/lsp.lua
+++ config/nvim/lua/selectnull/lsp.lua
@@ -83,7 +83,7 @@ end
 -- treesitter config
 packer.use {'nvim-treesitter/nvim-treesitter', run=':TSUpdate'}
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "python", "lua", "javascript" },
+  ensure_installed = { "python", "lua", "javascript", "typescript" },
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
