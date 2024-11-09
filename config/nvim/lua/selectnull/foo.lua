f7e8c9d7ae51362898efade3304d6b1d8b16b6fd
Sasha Matijasic <sasha@selectnull.com>
2024-11-09 11:38:43 +0100

    foo

--- config/nvim/lua/selectnull/foo.lua
+++ config/nvim/lua/selectnull/foo.lua
@@ -52,4 +52,39 @@ Module.get_win_size = function()
   return tostring(width) .. "x" .. tostring(height)
 end
 
+local function get_file_size(file_path)
+  local file = io.open(file_path, "r")
+  if not file then
+    return nil
+  end
+  local size = file:seek "end"
+  file:close()
+  return size
+end
+
+Module.get_file_stats = function()
+  local file_path = vim.api.nvim_buf_get_name(0)
+  print(file_path)
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  local width = 60
+  local height = 3
+  local win_height = vim.api.nvim_get_option "lines"
+  local win_width = vim.api.nvim_get_option "columns"
+
+  local opts = {
+    style = "minimal",
+    relative = "editor",
+    border = "single",
+    width = width,
+    height = height,
+    row = (win_height - height) * 0.5 - 1,
+    col = (win_width - width) * 0.5,
+  }
+  vim.api.nvim_open_win(buf, true, opts)
+  local lines = { "File: " .. file_path, "---", "Size:" .. get_file_size(file_path) }
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+  vim.api.nvim_buf_set_keymap(buf, "n", "<CR>", ":q<CR>", { noremap = true, silent = true })
+end
+
 return Module
