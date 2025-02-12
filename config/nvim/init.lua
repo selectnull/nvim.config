949aa50fc425c66e1c5913d94605ff0133fcb946
Sasha Matijasic <sasha@selectnull.com>
2025-02-12 15:38:06 +0100

    color schemes refactor

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -30,5 +30,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup "plugins"
 
+vim.cmd.colorscheme "kanagawa-wave"
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- config/nvim/lua/plugins/colorschemes.lua
+++ config/nvim/lua/plugins/colorschemes.lua
@@ -1,49 +1,34 @@
-return {
+local colors = {
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    no_init = function()
-      vim.cmd.colorscheme "rose-pine"
-    end,
   },
 
   {
     "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    no_init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme "tokyonight-night"
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi "Comment gui=none"
-    end,
   },
 
   {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    no_init = function()
-      vim.cmd.colorscheme "eldritch"
-    end,
+    "olimorris/onedarkpro.nvim",
   },
 
   {
     "rebelot/kanagawa.nvim",
-    colors = {
-      theme = {
-        all = {
-          ui = {
-            bg_gutter = "none",
+    config = function()
+      require("kanagawa").setup {
+        colors = {
+          theme = {
+            all = { ui = { bg_gutter = "none" } },
           },
         },
-      },
-    },
-    init = function()
-      vim.cmd.colorscheme "kanagawa-wave"
+      }
     end,
   },
 }
+
+-- all colors schemes use priority = 1000 in order to load first
+for _, v in pairs(colors) do
+  v.priority = 1000
+end
+
+return colors
