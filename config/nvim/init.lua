d1e08dd8a84796288bd99fcbd4e04321ff34d673
Sasha Matijasic <sasha@selectnull.com>
2025-02-12 15:56:06 +0100

    Switch lazy.nvim to structured setup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,8 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- see `:help mapleader`
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- my own configuration
 require "selectnull.vimopts"
 require "selectnull.keymaps"
 require "selectnull.emacs-insert-mode"
@@ -11,26 +10,11 @@ require "selectnull.make-css"
 require "selectnull.filetypes"
 require "selectnull.quickfix"
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- load plugins via lazy.nvim
+require "config.lazy"
 
-require("lazy").setup "plugins"
-
+-- see `:help colorscheme`
 vim.cmd.colorscheme "kanagawa-wave"
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- see `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
