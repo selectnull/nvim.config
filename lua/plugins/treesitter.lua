be3ad7e3ca14294ca12f6c5d57d5dbfe3686e9c1
Sasha Matijasic <sasha@selectnull.com>
2026-04-02 22:36:52 +0200

    Fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    -- setup tree-sitter
-    local configs = require "nvim-treesitter.configs"
-    configs.setup {
-      ensure_installed = { "lua", "python", "javascript" },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      -- Ignore Swift parser installation issues (use LSP instead)
-      ignore_install = { "swift" },
-    }
-  end,
+  branch = "main",
+  opts = {
+    ensure_installed = { "lua", "python", "javascript" },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
